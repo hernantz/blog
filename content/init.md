bad122b032089132529cf2365d3882b79617ec72
Hernan Lozano <hernantz@gmail.com>
2013-08-04 13:07:53 -0300

    Initial commit

---
+++ content/init.md
@@ -0,0 +1,18 @@
+Title: Init
+Date: 2013-07-03 18:06
+Category: Personal
+Tags: pelican, publishing
+Slug: init
+Author: hernantz 
+Summary: Short version for index and feeds
+
+This is my not-sexy new blog. It starts with a single post and a single 
+blog, but eventually would like this to become a planet of blogs.
+
+
+Why now? Becouse in the last weeks I felt the urge to write for myself. 
+Knowing my thoughts will be publicly available forces me to think wisely, 
+and arrive to better conclusions. 
+Aditionally, until now didn't like any of the blogging platforms out there, 
+with pelican I'm being able to craft my own posts with python and vim 
+is also is part of the motivation.
---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'hernantz'
+SITENAME = u'what\'s the point'
+SITEURL = ''
+
+TIMEZONE = 'America/Buenos_Aires'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/hernantz'),
+          ('Github', 'http://github.com/hernantz'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = '/home/hernantz/devel/pelican-themes/mnmlist/'
+
+SITESUBTITLE = 'Mostly lies and rants in plain text'
